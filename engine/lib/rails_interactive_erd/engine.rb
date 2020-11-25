7f9b8beabbd4c8d2577de6aa3560b4b03d24241f
Jack Swiggett <jackswiggett@gmail.com>
2020-11-25 00:01:05 -0800

    Precompile JavaScript asset

--- engine/lib/rails_interactive_erd/engine.rb
+++ engine/lib/rails_interactive_erd/engine.rb
@@ -1,5 +1,9 @@
 module RailsInteractiveErd
   class Engine < ::Rails::Engine
     isolate_namespace RailsInteractiveErd
+
+    initializer 'rails_interactive_erd.assets.precompile' do |app|
+      app.config.assets.precompile += %w[rails_interactive_erd_manifest.js]
+    end
   end
 end
--- engine/test/dummy/app/assets/config/manifest.js
+++ engine/test/dummy/app/assets/config/manifest.js
@@ -1,2 +1,1 @@
 //= link_tree ../images
-//= link rails_interactive_erd_manifest.js
